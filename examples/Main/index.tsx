61bd5a923b6b0e728023fea5e93f345f3d050a50
cpojer <christoph.pojer@gmail.com>
2023-11-15 21:24:41 +0900

    Get rid of all the cruft.

--- examples/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-import Main from './Main';
-
-const element = document.createElement('div');
-document.body.appendChild(element);
-
-render(<Main />, element);
